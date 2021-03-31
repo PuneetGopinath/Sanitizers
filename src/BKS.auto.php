fd796a10572c4ce8b008ae89787788c2612d6055
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2021-03-31 18:22:03 +0530

    Fix errors

--- src/BKS.auto.php
+++ src/BKS.auto.php
@@ -20,7 +20,7 @@ class SanitizerData
      * 
      * @var array
      */
-    public static $config = array(
+    public $config = array(
         "maxInputLength" => 100, // 1
         "encoding" => "UTF-8", // 2
         "preventXSS" => true // 3
